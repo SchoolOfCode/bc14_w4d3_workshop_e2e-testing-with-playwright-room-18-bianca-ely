13b227e977bdade1efec2924326fa3124fd2a49a
elydreaves <elydreaves@hotmail.co.uk>
2023-04-05 15:20:28 +0100

    progress task 2

--- tests/app.test.js
+++ tests/app.test.js
@@ -5,11 +5,21 @@ import { test, expect } from "@playwright/test";
 // check the url opens
 const url = 'http://localhost:3000/';
 // await page.goto('http://localhost:3000/');
+
 test("input field", async( {page}) => {
-    await page.goto(url);
+await page.goto(url);
+
+// check if input is empty
+//  locate input
+//  check input text value
+const input =  page.getByRole('textbox',{ name: 'New Todo:' })
+
+
+await expect(input).toBeEmpty();
 
 })
-// check if input is empty
+
+
 // write something into input
 // click the add button
 // check if list adds the input
