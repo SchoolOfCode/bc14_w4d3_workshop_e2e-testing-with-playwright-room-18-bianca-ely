02364a69c93d56802708a53c6ecc98df2fb8ddb5
bianca <bianca.g.buha@gmail.com>
2023-04-05 14:09:41 +0100

    task 1, started task 2

---
+++ tests/app.test.js
@@ -0,0 +1,26 @@
+// Plan
+import { test, expect } from "@playwright/test";
+
+
+// check the url opens
+const url = 'http://localhost:3000/';
+// await page.goto('http://localhost:3000/');
+test("input field", async( {page}) => {
+    await page.goto(url);
+
+})
+// check if input is empty
+// write something into input
+// click the add button
+// check if list adds the input
+
+// check the list item is not ticked
+// check the input field is empty
+
+// click the box, check if it gets ticked
+// click the box, check if it gets unticked
+
+// click the delete button
+// check if item gets deleted
+
+
